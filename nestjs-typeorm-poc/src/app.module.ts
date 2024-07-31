1090ed690a5c99ddf836e5e97c5d75f9b9f03604
mincheolc <mccha0407@gmail.com>
2024-07-31 16:56:43 +0900

    feat: typeORM으로 Mysql 연결 + 환경변수 외부 파일 (.env)사용

--- nestjs-typeorm-poc/src/app.module.ts
+++ nestjs-typeorm-poc/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 전역 모듈로 설정
+      envFilePath:
+        process.env.NODE_ENV === 'production' ? '.env.production' : '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('MYSQL_HOST'),
+        port: configService.get<number>('MYSQL_PORT'),
+        username: configService.get<string>('MYSQL_USERNAME'),
+        password: configService.get<string>('MYSQL_PASSWORD'),
+        database: configService.get<string>('MYSQL_DATABASE'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'], // 모든 엔티티 파일을 자동으로 로드
+        synchronize: configService.get<string>('NODE_ENV') !== 'production', // 개발 환경에서만 사용
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
